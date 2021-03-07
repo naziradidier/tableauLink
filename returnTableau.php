fb7a139ac5cc6da390bf47bdacafc0b3f694876b
naziradidier <nazirap20.aps1b@gmai.com>
2021-03-07 19:53:33 +0100

    page du tableau seulement

---
+++ returnTableau.php
@@ -0,0 +1,9 @@
+<?php 
+     $personne = array(
+         array('nom' => 'jind', 'age' => 25),
+         array('nom' => 'nazirah', 'age' => 20),
+         array('nom' => 'xxx', 'age' => 70) 
+     );
+
+    
+?>
