8267586b67d151722313426f78dbe08c10963226
Nazirah <74833519+naziradidier@users.noreply.github.com>
2021-03-12 10:52:24 +0100

    Update traitement2.html.php

--- traitement2.html.php
+++ traitement2.html.php
@@ -14,6 +14,7 @@
     if( isset($_GET['choix'] ) ){
         $i = $_GET['choix'];
 
+        if($i>=0){
         echo '<table>
             <tr>
                 <th>Nom</th>
@@ -21,17 +22,21 @@
                 <th>Age</th>
                 <th>Lieu</th>
             </tr>';
-  
-      
-          echo '<tr>';
-              echo '<td> '. $personne[$i]['nom'] .' </td>';
-              echo '<td>' . $personne[$i]['Prenom'] . '</td>';
-              echo '<td> '. $personne[$i]['age'] .' </td>';
-              echo '<td>' . $personne[$i]['lieu'] . '</td>';
-          echo '</tr>';
 
-// *******************************************************************************************************************
-          if ($i < count($personne) -1) {
+           
+                echo '<tr>';
+                    echo '<td> '. $personne[$i]['nom'] .' </td>';
+                    echo '<td>' . $personne[$i]['Prenom'] . '</td>';
+                    echo '<td> '. $personne[$i]['age'] .' </td>';
+                    echo '<td>' . $personne[$i]['lieu'] . '</td>';
+                echo '</tr>';
+           
+
+          echo '</table>';
+        }
+
+// ******************************************************************************************************************
+          if ($i >= 0 and $i < count($personne) -1) {
             echo '<table>
         
             <tr>
@@ -39,43 +44,47 @@
             </tr>';
 
             echo '<tr>';
-            echo '<td> '. $personne[$i ++]['nom'] .'</td>';
+            echo '<td> '. $personne[$i++]['nom'] .'</td>';
              echo '</tr>';
 
              echo '<tr>';
-             echo '<td> <a href="traitement2.html.php?choix=' . $i .'"> <button type="submit">'. "next" .'</button> </a> </td>';
+             echo '<td> <a href="traitement2.html.php?choix=' . $i  .'"> <button type="submit">'. "next" .'</button> </a> </td>';
              echo '</tr>';
 
              echo '</table>';
  
         }
-        elseif($i >= count($personne)){
+        
+        if($i >= 0 and $i < count($personne) -1){
+            // echo  "we are in the first item";
             echo '<table>
                         <tr>
                             <th>Nom</th>
                         </tr>';
 
                     echo '<tr>';
-                    echo '<td> '. $personne[$i --]['nom'] .'</td>';
+                    echo '<td> '. $personne[$i--]['nom'] .'</td>';
                     echo '</tr>';
 
                     echo '<tr>';
-                    echo '<td> <a href="traitement2.html.php?choix=' . $i .'"> <button type="submit">'. "previous" .'</button> </a> </td>';
+                    echo '<td> <a href="traitement2.html.php?choix=' . $i -1 .'"> <button type="submit">'. "previous" .'</button> </a> </td>';
                     echo '</tr>';
+             echo '</table>';
         }
         else{
+            echo '<table>';
             echo '<tr>';
-            echo '<td> '. 'y en a plus' .'</td>';
+            echo '<td> '. 'la fin de liste' .' </td>';
             echo '</tr>';
+            echo '</table>';
         } 
-
-      echo '</table>';
    }
-   
+   echo '<table>';
+        echo '<tr>';
+        echo '<td> <a href="traitement1.php"> <button type="submit">'. "retour" .'</button> </a> </td>';
+        echo '</tr>';
+   echo '</table>';
 ?>
-        <button type="submit">
-        <a href="http://localhost/test/traitement1.php" ><p>retour</p></a>
-        </button>
 
 </body>
 </html>
